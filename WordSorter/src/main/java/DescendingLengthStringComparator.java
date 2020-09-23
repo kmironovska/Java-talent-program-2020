f9518da7709604af813b9ea5ece4228dd5f7cd64
Kristina Mironovska <37073570+kmironovska@users.noreply.github.com>
2020-09-23 23:06:26 +0200

    Add files via upload

---
+++ WordSorter/src/main/java/DescendingLengthStringComparator.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class DescendingLengthStringComparator implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        return o2.length() - o1.length();
+    }
+}
