96310bc969dc9047ce9a64cd79329b8f2ba76a7f
Kristina Mironovska <37073570+kmironovska@users.noreply.github.com>
2020-10-02 19:49:00 +0200

    Update 08-data-fetching.js

--- Exercises-08/java-talent-2020-fe-react-master/src/exercises/08-data-fetching.js
+++ Exercises-08/java-talent-2020-fe-react-master/src/exercises/08-data-fetching.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import './04-composition.css';
+import PropTypes from "prop-types";
 // For our data fetching exercise we're gonna take advantage of the Notes REST API that you already created.
 //
 // It is best to use lifecycle method `componentDidMount` to
@@ -13,12 +16,78 @@ import React, { Component } from 'react';
 //
 
 export default class NotesGrid extends Component {
+
+    state = {
+        notes: []
+    }
+    componentDidMount() {
+        axios.get('/api/notes')
+            .then(response => {
+                this.setState({
+                    notes : response.data
+                });
+            });
+    }
+
     render() {
         return (
-            <div>
-                Notes! Render the Grid of notes in here.
-            </div>
+            <ul>
+                { this.state.notes.map(note =>
+                    <li key={note.id}> <Note title={note.title} content={note.content} tags={note.tags}/> </li>) }
+            </ul>
         );
     }
+
+
 }
+function Note(props) {
+
+    return <div className="note">
+        <NoteHeader title={props.title}/>
+        <NoteContent content={props.content}/>
+        <NoteFooter tags={props.tags}/>
+    </div>;
+}
+
+Note.propTypes = {
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired
+};
+
+function NoteHeader({ title }) {
+    return (
+        <div className="note-header">
+            {title}
+        </div>
+    );
+}
+
+NoteHeader.propTypes = {
+    title: PropTypes.string.isRequired
+};
+
+function NoteContent({ content }) {
+    return (
+        <div className="note-content">
+            {content}
+        </div>
+    );
+}
+
+NoteContent.propTypes = {
+    content: PropTypes.string.isRequired
+};
+function NoteFooter({ tags }) {
+    return (
+        <div className="note-footer">
+            {tags.map(
+                tag =><p>#{tag.name}</p>)}
+        </div>
+    );
+}
+
+NoteFooter.propTypes = {
+};
+
+
 export const Example = () => <NotesGrid />;
