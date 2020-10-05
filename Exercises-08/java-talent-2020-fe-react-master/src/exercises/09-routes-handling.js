dcdee52d365b27a42ca0d4db48dcb9d3d6bf0e84
Kristina Mironovska <37073570+kmironovska@users.noreply.github.com>
2020-10-05 15:51:07 +0200

    Update 09-routes-handling.js

--- Exercises-08/java-talent-2020-fe-react-master/src/exercises/09-routes-handling.js
+++ Exercises-08/java-talent-2020-fe-react-master/src/exercises/09-routes-handling.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 // Pretty much every application is going to need to do something with navigation and routes handling. 
 // Since React doesn't include routing mechanism we will use additional library named 'react-router'. 
 // As you can see in this application there is already Routing defined and on each exercise the path in the browser 
@@ -13,16 +13,42 @@ import React from 'react';
 //   Make sure that you use the current path '/exercise/routes-handling' when defining your custom Routes.
 
 function Routes() {
-    return 'Incomplete';
+    return (
+
+        <Router>
+            <div className="container">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/exercise/routes-handling/edit-note">Edit Note</Link>
+                        </li>
+                        <li>
+                            <Link to="/exercise/routes-handling/create-note">Create Note</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/exercise/routes-handling/edit-note">
+                        <EditNote/>
+                    </Route>
+                    <Route path="/exercise/routes-handling/create-note">
+                        <CreateNote/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+
+    );
 }
 
 function EditNote() {
-    return 'Edit Note Component';
+    return <h1>This is a Note Compoment</h1>
+
 }
 
 function CreateNote() {
-    return 'Create Note Component';
+    return <h1>Create note component</h1>
 }
 
 
-export const Example = () => <Routes />;
+export const Example = () => <Routes/>;
